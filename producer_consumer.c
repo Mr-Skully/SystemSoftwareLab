cad3de54d027556bdcf2ce1b4c8beb11478837af
Emil Titus <emiltitus.inc@gmail.com>
2020-09-27 12:53:08 +0530

    Complete program

--- producer_consumer.c
+++ producer_consumer.c
@@ -1,6 +1,10 @@
 #include <pthread.h>                                                // for creating threads
 #include <stdio.h>                                                  // for standard I/O
+#include <stdlib.h>                                                 // for exit()
 #include <windows.h>                                                // for Sleep(microseconds) in Windows; use unistd.h for sleep(seconds) in Linux
+#define MAX_ITERATIONS 10
+
+FILE *input, *output;                                               // input and output file pointers
 
 pthread_mutex_t mutex;                                              // mutex variable
 pthread_cond_t cond;                                                // condition variable use for blocking and waking threads
@@ -8,14 +12,14 @@ pthread_cond_t cond;                                                // condition
 boolean cannot_consume = FALSE;                                     // booleans used to avoid any infinite loops
 boolean cannot_produce = FALSE;                                     // booleans used to avoid any infinite loops
 
-int n = 3;                                                          // no. of elements in the production_rate and consumption_rate arrays
-int production_rate[] = {3, 7, 9};                                 // production rate of producer at each stage
+int n = 0;                                                          // no. of elements in the production_rate and consumption_rate arrays
+int production_rate[MAX_ITERATIONS];                                // production rate of producer at each stage
 int p = 0;                                                          // stage of production, used to index the production_rate array
-int consumption_rate[] = {4, 3, 8};                                // consumption rate of consumer at each stage
+int consumption_rate[MAX_ITERATIONS];                               // consumption rate of consumer at each stage
 int c = 0;                                                          // stage of consumption, used to index the consumption_rate array
 
 int buffer = 0;                                                     // current size of buffer
-int buffer_size = 20;                                               // max size of buffer
+int buffer_size = 0;                                                // max size of buffer
 
 void * producer(){                                                  // producer - thread
     while (p < n) {                                                 // loop till the production_rate array is traversed
@@ -25,6 +29,8 @@ void * producer(){                                                  // producer 
             buffer += production_rate[p++];                         // update buffer size to simulate production
             printf("\nProducer Produced Successfully");
             printf("\nBuffer Spaces Filled: %d", buffer);           // display current buffer size
+            fprintf(output, "\nProducer Produced Successfully");    // write the same details to output file
+            fprintf(output, "\nBuffer Spaces Filled: %d", buffer);
             pthread_mutex_unlock(&mutex);                           // unlock mutex
             pthread_cond_signal(&cond);                             // signal the consumer process
             Sleep(10);                                              // wait for 10ms, giving time for consumer to lock mutex if required
@@ -34,6 +40,8 @@ void * producer(){                                                  // producer 
             if (buffer == buffer_size) {                            // if buffer is full
                 printf("\nProducer Cannot Produce: Buffer Full");
                 printf("\nBuffer Spaces Filled: %d", buffer);       // display buffer size
+                fprintf(output, "\nProducer Cannot Produce: Buffer Full");// write the same details to output file
+                fprintf(output, "\nBuffer Spaces Filled: %d", buffer);
             }
             pthread_cond_signal(&cond);                             // signal consumer
             Sleep(10);                                              // sleep for 10ms
@@ -54,6 +62,8 @@ void * consumer(){                                                  // consumer 
             if (!buffer) {                                          // when buffer is empty
                 printf("\nConsumer Cannot Consume: Buffer Empty");
                 printf("\nBuffer Spaces Filled: %d", buffer);       // display buffer size
+                fprintf(output, "\nConsumer Cannot Consume: Buffer Empty");// write the same details to output file
+                fprintf(output, "\nBuffer Spaces Filled: %d", buffer);
             }
             if (cannot_produce)                                     // terminate the thread when producer cannot produce and consumer cannot consume
                 pthread_exit(NULL);
@@ -64,6 +74,8 @@ void * consumer(){                                                  // consumer 
             buffer -= consumption_rate[c++];                        // simulate consumption by updating buffer size
             printf("\nConsumer Consumed Successfully");
             printf("\nBuffer Spaces Filled: %d", buffer);           // display buffer size
+            fprintf(output, "\nConsumer Consumed Successfully");    // write the same details to output file
+            fprintf(output, "\nBuffer Spaces Filled: %d", buffer);
         }
     }
     pthread_mutex_unlock(&mutex);                                   // unlock mutex
@@ -72,6 +84,8 @@ void * consumer(){                                                  // consumer 
 }
 
 int main(int argc, char const *argv[]) {
+    int i;                                                          // counter for loops
+
     pthread_t producer_thread, consumer_thread;                     // threads
     pthread_attr_t attr;                                            // thread attributes
 
@@ -80,11 +94,52 @@ int main(int argc, char const *argv[]) {
     pthread_attr_init(&attr);                                       // initialize thread attributes with default values
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);    // make the threads "joinable"
 
+    if(argc != 2) {                                                 // display documentation if no input file is specified
+        printf("\nUSAGE: %s INPUT_FILENAME\n", argv[0]);
+
+        printf("\nINPUT:\n   A text file.");
+        printf("\n   The first line contains a positive integer");
+        printf("\n   which denotes the maximum possible size of the buffer.");
+        printf("\n   The next line contains a positive integer 'n',");
+        printf("\n   which denotes number of times the producer/consumer can");
+        printf("\n   produce/consume into/from the buffer.");
+        printf("\n   The next line 'n' contains one integer on each line,");
+        printf("\n   each integer denoting the production rate of the producer");
+        printf("\n   each time it is called to produce into the buffer.");
+        printf("\n   The next line 'n' contains one integer on each line,");
+        printf("\n   each integer denoting the consumption rate of the consumer");
+        printf("\n   each time it is called to consume from the buffer.");
+
+        printf("\nOUTPUT:\n   A text file, \"pc_output.txt\".\n   The information written to the file is also displayed on screen.\n   If the file doesn't exist, a new file will be automatically created.\n   If the file exists, the new output will overwrite the original file.\n\n");
+        exit(0);
+    }
+
+    input = fopen(argv[1], "r");                                    // open input file
+    if(!input){                                                     // if unable to open file
+        printf("\n[.] Could not open %s.\n\n", argv[1]);
+        exit(1);
+    }
+    fscanf(input, "%d", &buffer_size);                              // read buffer size from file
+    fscanf(input, "%d", &n);                                        // read no. of producer/consumer will run from file
+    for (i = 0; i < n; i++)                                         // read production rate array
+        fscanf(input, "%d", &production_rate[i]);
+    for (i = 0; i < n; i++)                                         // read consumption rate array
+        fscanf(input, "%d", &consumption_rate[i]);
+    fclose(input);
+
+    output = fopen(".\\pc_output.txt", "w");                        // open file for writing output
+    if(!output){
+        printf("\n[.] Could not open output file.\n");
+        exit(1);
+    }
+
     pthread_create(&consumer_thread, &attr, consumer, NULL);        // create the consumer thread
     pthread_create(&producer_thread, &attr, producer, NULL);        // create the producer thread
 
     pthread_join(producer_thread, NULL);                            // ensure that the producer finished execution
     pthread_join(consumer_thread, NULL);                            // ensure that the consumer finished execution
+
+    fclose(output);
 
     pthread_attr_destroy(&attr);                                    // destroy thread attributes
     pthread_mutex_destroy(&mutex);                                  // destroy mutex
